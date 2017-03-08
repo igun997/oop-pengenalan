f01dde07ed48b0f7cd0bb12426f33572c89cebca
Indra Gunanda <indra.gunanda@gmail.com>
2017-03-08 17:57:09 +0700

    Materi Pertemuan 2, 08 Maret 2017

---
+++ src/pengenalanoop/PengenalanOOP.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pengenalanoop;
+
+/**
+ *
+ * @author SystemFive
+ */
+public class PengenalanOOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       input dia = new input();
+       cetak test = new cetak();
+       dia.setUmur(2);
+       test.cetak_identitas();
+    }
+    
+}
---
+++ src/pengenalanoop/input.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pengenalanoop;
+
+/**
+ *
+ * @author SystemFive
+ */
+public class input {
+
+    /**
+     *
+     * @param a
+     */
+    private int umur; // Private Modifier
+    private int kelas; // Protected
+    
+    public int setUmur(int umur)
+    {
+        return umur;
+    }
+    public int getUmur()
+    {
+        return umur;
+    }
+    
+}
